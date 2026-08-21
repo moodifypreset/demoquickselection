dd60ccc4c144982dd2ded059f2f62188860f0a1a
moodifypreset <moodifypreset@gmail.com>
2026-08-21 11:01:18 +0700

    Update pengaturan.js

--- pengaturan.js
+++ pengaturan.js
@@ -4,10 +4,16 @@
 // =========================================================================
 
 const CONFIG_APP = {
-    // 1. Nama Studio Foto Anda (Akan tampil di header dan portal klien)
+    // 1. Nama Studio Foto Anda
     namaVendor: "Moodify Preset",
 
-    // 2. Google Drive API Key (Penting agar galeri bisa memuat foto)
+    // 2. Google Drive API Key
     googleApiKey: "AIzaSyAZczbLeVTXl-QKqLDJFWQLCd-lW9jqqxo",
-    whatsappAdmin: "6282117102831" // <-- TAMBAHKAN BARIS INI (Ganti dengan nomor WA admin)
+    
+    // 3. Nomor WhatsApp Admin
+    whatsappAdmin: "6282117102831",
+
+    // 4. Konfigurasi Supabase (Untuk Demo Mode)
+    supabaseUrl: "https://ygoawyedjxcxxnhlbtit.supabase.co",
+    supabaseKey: "sb_publishable_El7jjrn0WyxR09b8wRkdoQ_BDFhXXa-"
 };
